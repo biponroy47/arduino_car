539797e8dce664af44a3a5793825f58a54045b5e
Bipon Roy <biponroy47@yahoo.com>
2024-07-17 21:55:37 -0400

    fixed code

--- code/code.ino
+++ code/code.ino
@@ -1,13 +1,13 @@
-#include <IRremote.hpp>
-
-int IR_RECEIVE_PIN = A0;
+#include <IRremote.h>
+int IR_RECEIVE_PIN = 11;
+IRrecv irrecv(IR_RECEIVE_PIN);
 
 int m1 = 3;
 int m1a = 4;
 int m1b = 2;
 
 int m2 = 6;
-int m2a = 11;
+int m2a = A1;
 int m2b = 9;
 
 int m3 = 5;
@@ -17,12 +17,10 @@ int m3b = 7;
 int m4 = 10;
 int m4a = 13;
 int m4b = 12;
-    
-void setup()
-{
+
+void setup() {
   Serial.begin(9600);
-  pinMode(IR_RECEIVE_PIN, INPUT);
-  IrReceiver.begin(IR_RECEIVE_PIN);
+  irrecv.enableIRIn();
   pinMode(m1, OUTPUT);
   pinMode(m1a, OUTPUT);
   pinMode(m1b, OUTPUT);
@@ -35,97 +33,86 @@ void setup()
   pinMode(m4, OUTPUT);
   pinMode(m4a, OUTPUT);
   pinMode(m4b, OUTPUT);
-  
 }
 
-void loop()
-{
-
-  Serial.println("fwd");
+void loop() {
+  if (irrecv.decode()) {
+    //UP
+    if (irrecv.decodedIRData.decodedRawData == 3108437760) {
+      Serial.print("FORWARD");
       digitalWrite(m1, HIGH);
       digitalWrite(m1a, LOW);
-  	  digitalWrite(m1b, HIGH);
-  	  digitalWrite(m2, HIGH);
-  	  digitalWrite(m2a, HIGH);
-  	  digitalWrite(m2b, LOW);
-  	  digitalWrite(m3, HIGH);
-  	  digitalWrite(m3a, LOW);
-  	  digitalWrite(m3b, HIGH);
-  	  digitalWrite(m4, HIGH);
-  	  digitalWrite(m4a, LOW);
-  	  digitalWrite(m4b, HIGH);
-
-
-  // if (IrReceiver.decode()) {
-  //   if(IrReceiver.decodedIRData.decodedRawData == 4261527296){
-  //     Serial.println("fwd");
-  //     digitalWrite(m1, HIGH);
-  //     digitalWrite(m1a, LOW);
-  // 	  digitalWrite(m1b, HIGH);
-  // 	  digitalWrite(m2, HIGH);
-  // 	  digitalWrite(m2a, LOW);
-  // 	  digitalWrite(m2b, HIGH);
-  // 	  digitalWrite(m3, HIGH);
-  // 	  digitalWrite(m3a, LOW);
-  // 	  digitalWrite(m3b, HIGH);
-  // 	  digitalWrite(m4, HIGH);
-  // 	  digitalWrite(m4a, LOW);
-  // 	  digitalWrite(m4b, HIGH);
-  //   }
-  //   else if(IrReceiver.decodedIRData.decodedRawData == 4211392256){
-  //     Serial.println("left");
-  //     digitalWrite(m1, HIGH);
-  //     digitalWrite(m1a, LOW);
-  // 	  digitalWrite(m1b, HIGH);
-  // 	  digitalWrite(m2, HIGH);
-  // 	  digitalWrite(m2a, HIGH);
-  // 	  digitalWrite(m2b, LOW);
-  // 	  digitalWrite(m3, HIGH);
-  // 	  digitalWrite(m3a, LOW);
-  // 	  digitalWrite(m3b, HIGH);
-  // 	  digitalWrite(m4, HIGH);
-  // 	  digitalWrite(m4a, HIGH);
-  // 	  digitalWrite(m4b, LOW);
-  //   }
-  //   else if(IrReceiver.decodedIRData.decodedRawData == 4177968896){
-  //     Serial.println("right");
-  //     digitalWrite(m1, HIGH);
-  //     digitalWrite(m1a, HIGH);
-  // 	  digitalWrite(m1b, LOW);
-  // 	  digitalWrite(m2, HIGH);
-  // 	  digitalWrite(m2a, HIGH);
-  // 	  digitalWrite(m2b, LOW);
-  // 	  digitalWrite(m3, HIGH);
-  // 	  digitalWrite(m3a, HIGH);
-  // 	  digitalWrite(m3b, LOW);
-  // 	  digitalWrite(m4, HIGH);
-  // 	  digitalWrite(m4a, HIGH);
-  // 	  digitalWrite(m4b, LOW);
-  //   }
-  //   else if(IrReceiver.decodedIRData.decodedRawData == 4127833856){
-  //     Serial.println("back");
-  //     digitalWrite(m1, HIGH);
-  //     digitalWrite(m1a, HIGH);
-  // 	  digitalWrite(m1b, LOW);
-  // 	  digitalWrite(m2, HIGH);
-  // 	  digitalWrite(m2a, HIGH);
-  // 	  digitalWrite(m2b, LOW);
-  // 	  digitalWrite(m3, HIGH);
-  // 	  digitalWrite(m3a, HIGH);
-  // 	  digitalWrite(m3b, LOW);
-  // 	  digitalWrite(m4, HIGH);
-  // 	  digitalWrite(m4a, HIGH);
-  // 	  digitalWrite(m4b, LOW); 
-  //   }
-  //   else {
-  //     Serial.println("stop");
-  //     Serial.println(IrReceiver.decodedIRData.decodedRawData);
-  //     digitalWrite(m1, LOW);
-  //     digitalWrite(m2, LOW);
-  //     digitalWrite(m3, LOW);
-  //     digitalWrite(m4, LOW);
-  //   }
-  // }
-  // IrReceiver.resume();
+      digitalWrite(m1b, HIGH);
+      digitalWrite(m2, HIGH);
+      digitalWrite(m2a, HIGH);
+      digitalWrite(m2b, LOW);
+      digitalWrite(m3, HIGH);
+      digitalWrite(m3a, LOW);
+      digitalWrite(m3b, HIGH);
+      digitalWrite(m4, HIGH);
+      digitalWrite(m4a, HIGH);
+      digitalWrite(m4b, LOW);
+    } 
+    //LEFT
+    else if (irrecv.decodedIRData.decodedRawData == 3141861120) {
+      Serial.print("LEFT");
+      digitalWrite(m1, HIGH);
+      digitalWrite(m1a, HIGH);
+      digitalWrite(m1b, LOW);
+      digitalWrite(m2, HIGH);
+      digitalWrite(m2a, HIGH);
+      digitalWrite(m2b, LOW);
+      digitalWrite(m3, HIGH);
+      digitalWrite(m3a, HIGH);
+      digitalWrite(m3b, LOW);
+      digitalWrite(m4, HIGH);
+      digitalWrite(m4a, HIGH);
+      digitalWrite(m4b, LOW);
+    } 
+    //RIGHT
+    else if (irrecv.decodedIRData.decodedRawData == 3158572800) {
+      Serial.print("RIGHT");
+      digitalWrite(m1, HIGH);
+      digitalWrite(m1a, LOW);
+      digitalWrite(m1b, HIGH);
+      digitalWrite(m2, HIGH);
+      digitalWrite(m2a, LOW);
+      digitalWrite(m2b, HIGH);
+      digitalWrite(m3, HIGH);
+      digitalWrite(m3a, LOW);
+      digitalWrite(m3b, HIGH);
+      digitalWrite(m4, HIGH);
+      digitalWrite(m4a, LOW);
+      digitalWrite(m4b, HIGH);
+    } 
+    //DOWN
+    else if (irrecv.decodedIRData.decodedRawData == 3927310080) {
+      Serial.print("FORWARD");
+      digitalWrite(m1, HIGH);
+      digitalWrite(m1a, HIGH);
+      digitalWrite(m1b, LOW);
+      digitalWrite(m2, HIGH);
+      digitalWrite(m2a, LOW);
+      digitalWrite(m2b, HIGH);
+      digitalWrite(m3, HIGH);
+      digitalWrite(m3a, HIGH);
+      digitalWrite(m3b, LOW);
+      digitalWrite(m4, HIGH);
+      digitalWrite(m4a, LOW);
+      digitalWrite(m4b, HIGH);
+    } 
+    //MIDDLE
+    else if (irrecv.decodedIRData.decodedRawData == 3208707840) {
+      Serial.println("STOP");
+      digitalWrite(m1, LOW);
+      digitalWrite(m2, LOW);
+      digitalWrite(m3, LOW);
+      digitalWrite(m4, LOW);
+    }
+    else {
+      Serial.println(irrecv.decodedIRData.decodedRawData);
+    }
+    delay(200);
+  }
+  irrecv.resume();
 }
-   
